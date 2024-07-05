4aba3cd3202af8e906faa2edccf35cf953db9701
hieule2108 <levanhieusona@gmail.com>
2024-07-05 18:11:14 +0700

    Fix bug Email_Address Type

--- my-app/src/app/(dashboard)/page.tsx
+++ my-app/src/app/(dashboard)/page.tsx
@@ -1,14 +1,8 @@
 import CourseItem from "@/components/Course/CourseItem";
 import { CourseGrid } from "@/components/common";
 import Heading from "@/components/typography/Heading";
-import createUser from "@/lib/actions/user.actions";
 
 export default async function page() {
-  const user = await createUser({
-    clerkId: "clerk_123",
-    email_address: "levanhieusona@gmail.com",
-    username: "vanhieu",
-  });
   return (
     <div>
       <Heading>Khám phá</Heading>
