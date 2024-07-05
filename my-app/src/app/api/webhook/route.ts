7a5f9451a11eca0eaa8c09ce2884728ac9456f3b
hieule2108 <levanhieusona@gmail.com>
2024-07-05 18:06:42 +0700

    redeloy webhook

--- my-app/src/app/api/webhook/route.ts
+++ my-app/src/app/api/webhook/route.ts
@@ -1,13 +1,21 @@
+import createUser from "@/lib/actions/user.actions";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
 export async function POST(req: Request) {
-  const svix_id = req.headers.get("svix-id") ?? "";
-  const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
-  const svix_signature = req.headers.get("svix-signature") ?? "";
+  const svix_id = headers().get("svix-id") ?? "";
+  const svix_timestamp = headers().get("svix-timestamp") ?? "";
+  const svix_signature = headers().get("svix-signature") ?? "";
   if (!process.env.WEBHOOK_SECRET) {
     throw new Error("WEBHOOK_SECRET is not set");
   }
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Bad Request", { status: 400 });
+  }
+
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
@@ -28,7 +36,18 @@ export async function POST(req: Request) {
   const eventType = msg.type;
   if (eventType === "user.created") {
     // create user to database
-    console.log(msg.data);
+    const { id, username, email_addresses, image_url } = msg.data;
+    const user = await createUser({
+      username: username!,
+      name: username!,
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      avatar: image_url,
+    });
+    return NextResponse.json({
+      message: "OK",
+      user,
+    });
   }
   // Rest
 
